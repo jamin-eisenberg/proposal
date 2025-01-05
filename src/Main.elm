7c5c93119c7e45a55f04b43f106da31ad7e41ef5
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2025-01-05 16:33:39 -0500

    End and manual override

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,27 @@
 port module Main exposing (main)
 
 import Browser
-import Html exposing (img, text)
-import Html.Attributes exposing (src, style, width)
+import Html exposing (button, div, img, input, text)
+import Html.Attributes exposing (class, src, style, type_, value, width)
+import Html.Events exposing (onClick, onInput)
 import String
 import Time
 
 
 type alias Model =
-    { step : Int }
+    { step : Maybe Int, pendingPassword : String }
 
 
 type Msg
     = ScanSucceeded NfcTag
     | ScanFailed String -- reason
     | ManualOverride
+    | UpdatePendingPassword String
 
 
 type NfcTag
     = Bench
+    | SteeringWheel
 
 
 port scanSucceeded : (Int -> msg) -> Sub msg
@@ -50,6 +53,9 @@ stepNfcTag step =
         0 ->
             Just Bench
 
+        1 ->
+            Just SteeringWheel
+
         _ ->
             Nothing
 
@@ -69,33 +75,65 @@ main =
         }
 
 
+init : a -> ( Model, Cmd msg )
 init _ =
-    ( { step = 0 }, Cmd.none )
+    ( { step = Just 0, pendingPassword = "" }, Cmd.none )
 
 
+view : Model -> Html.Html Msg
 view model =
-    img [ src <| "images/" ++ String.fromInt model.step ++ ".png", style "width" "100%" ] []
+    case model.step |> Maybe.andThen stepNfcTag of
+        Nothing ->
+            text "You win!"
+
+        Just step ->
+            div []
+                [ input [ type_ "number", value model.pendingPassword, onInput UpdatePendingPassword ] []
+                , button [ class "button button-primary", onClick ManualOverride ] [ text "Manual Override" ]
+                , img [ src <| "images/" ++ Debug.toString step ++ ".png", style "width" "100%" ] []
+                ]
 
 
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
-    case msg of
-        ScanSucceeded nfcTag ->
-            let
-                _ =
-                    Debug.log "" nfcTag
-            in
-            if stepNfcTag model.step == Just nfcTag then
-                ( { model | step = model.step + 1 }, Cmd.none )
-
-            else
-                ( model, Cmd.none )
-
-        ScanFailed reason ->
-            let
-                _ =
-                    Debug.log "" reason
-            in
+    case model.step of
+        Nothing ->
             ( model, Cmd.none )
 
-        ManualOverride ->
-            ( { model | step = model.step + 1 }, Cmd.none )
+        Just step ->
+            case msg of
+                ScanSucceeded nfcTag ->
+                    let
+                        _ =
+                            Debug.log "" nfcTag
+                    in
+                    if
+                        stepNfcTag step
+                            == Just nfcTag
+                    then
+                        ( { model | step = Maybe.map ((+) 1) model.step }, Cmd.none )
+
+                    else
+                        ( model, Cmd.none )
+
+                ScanFailed reason ->
+                    let
+                        _ =
+                            Debug.log "" reason
+                    in
+                    ( model, Cmd.none )
+
+                ManualOverride ->
+                    if model.pendingPassword == manualOverridePassword then
+                        ( { model | step = Just <| step + 1, pendingPassword = "" }, Cmd.none )
+
+                    else
+                        ( { model | pendingPassword = "" }, Cmd.none )
+
+                UpdatePendingPassword newPassword ->
+                    ( { model | pendingPassword = newPassword }, Cmd.none )
+
+
+manualOverridePassword : String
+manualOverridePassword =
+    "6372"
